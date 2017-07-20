539512aa6e93b046e728e5f4a99cab3dbb6ddc61
Sam <sam.iagd@hotmail.com>
2017-07-20 20:59:05 +0800

    add stringutil library

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"github.com/0x0010/xgo/stringutil"
+)
 func main() {
 	fmt.Printf("Hello, world.\n")
+	fmt.Println(stringutil.Reverse("!oG olleH :gnirts desreveR"))
 }
