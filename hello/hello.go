1940e31f10840ac1947689f5022f0c7eb63eca4c
Sam <sam.iagd@hotmail.com>
2017-07-22 18:17:46 +0800

    1. call method from main package 2. define two go files in the same package 3. define tow functions under one package

--- hello/hello.go
+++ hello/hello.go
@@ -7,4 +7,14 @@ import (
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Println(stringutil.Reverse("!oG olleH :gnirts desreveR"))
+	fmt.Println(Reverse("!oG olleH :litu egakcap evitan gnisu gnirts desreveR"))
+	fmt.Println(stringutil.Split("Split string from split function"))
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
---
+++ stringutil/split.go
@@ -0,0 +1,6 @@
+package stringutil
+
+// split string
+func Split(s string) string {
+	return string(s);
+}
